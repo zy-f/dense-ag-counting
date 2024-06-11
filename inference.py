1d180b38010ed5d123396246123646978619968d
zy-f <ca@lcpmail.org>
2024-06-11 09:53:05 -0700

    inference/demo script

---
+++ inference.py
@@ -0,0 +1,389 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+import cv2
+
+WEIGHT_FILE = 'best-weights.pth'
+
+### vgg backbone arch
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=1000, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_layers(cfg, batch_norm=False, sync=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                if sync:
+                    print('use sync backbone')
+                    layers += [conv2d, nn.SyncBatchNorm(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+def vgg16_bn():
+    r"""VGG 16-layer model (configuration "D") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    CFG_D = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+    # return _vgg('vgg16_bn', 'D', True, pretrained, progress, sync=sync, **kwargs)
+    return VGG(make_layers(CFG_D, batch_norm=True, sync=False))
+
+class Backbone_VGG16BN(nn.Module):
+    def __init__(self, num_channels=256):
+        super().__init__()
+        backbone = vgg16_bn()
+        features = list(backbone.features.children())
+        self.body1 = nn.Sequential(*features[:13])
+        self.body2 = nn.Sequential(*features[13:23])
+        self.body3 = nn.Sequential(*features[23:33])
+        self.body4 = nn.Sequential(*features[33:43])
+        self.num_channels = num_channels
+        self.return_interm_layers = True
+
+    def forward(self, tensor_list):
+        out = []
+
+        if self.return_interm_layers:
+            xs = tensor_list
+            for _, layer in enumerate([self.body1, self.body2, self.body3, self.body4]):
+                xs = layer(xs)
+                out.append(xs)
+
+        else:
+            xs = self.body(tensor_list)
+            out.append(xs)
+        return out
+
+### p2pnet
+# the network frmawork of the regression branch
+class RegressionModel(nn.Module):
+    def __init__(self, num_features_in, num_anchor_points=4, feature_size=256):
+        super(RegressionModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act4 = nn.ReLU()
+
+        self.output = nn.Conv2d(feature_size, num_anchor_points * 2, kernel_size=3, padding=1)
+    # sub-branch forward
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.act1(out)
+
+        out = self.conv2(out)
+        out = self.act2(out)
+
+        out = self.output(out)
+
+        out = out.permute(0, 2, 3, 1)
+
+        return out.contiguous().view(out.shape[0], -1, 2)
+
+# the network frmawork of the classification branch
+class ClassificationModel(nn.Module):
+    def __init__(self, num_features_in, num_anchor_points=4, num_classes=80, prior=0.01, feature_size=256):
+        super(ClassificationModel, self).__init__()
+
+        self.num_classes = num_classes
+        self.num_anchor_points = num_anchor_points
+
+        self.conv1 = nn.Conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act4 = nn.ReLU()
+
+        self.output = nn.Conv2d(feature_size, num_anchor_points * num_classes, kernel_size=3, padding=1)
+        self.output_act = nn.Sigmoid()
+    # sub-branch forward
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.act1(out)
+
+        out = self.conv2(out)
+        out = self.act2(out)
+
+        out = self.output(out)
+
+        out1 = out.permute(0, 2, 3, 1)
+
+        batch_size, width, height, _ = out1.shape
+
+        out2 = out1.view(batch_size, width, height, self.num_anchor_points, self.num_classes)
+
+        return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+
+# generate the reference points in grid layout
+def generate_anchor_points(stride=16, row=3, line=3):
+    row_step = stride / row
+    line_step = stride / line
+
+    shift_x = (np.arange(1, line + 1) - 0.5) * line_step - stride / 2
+    shift_y = (np.arange(1, row + 1) - 0.5) * row_step - stride / 2
+
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+
+    anchor_points = np.vstack((
+        shift_x.ravel(), shift_y.ravel()
+    )).transpose()
+
+    return anchor_points
+# shift the meta-anchor to get an acnhor points
+def shift(shape, stride, anchor_points):
+    shift_x = (np.arange(0, shape[1]) + 0.5) * stride
+    shift_y = (np.arange(0, shape[0]) + 0.5) * stride
+
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+
+    shifts = np.vstack((
+        shift_x.ravel(), shift_y.ravel()
+    )).transpose()
+
+    A = anchor_points.shape[0]
+    K = shifts.shape[0]
+    all_anchor_points = (anchor_points.reshape((1, A, 2)) + shifts.reshape((1, K, 2)).transpose((1, 0, 2)))
+    all_anchor_points = all_anchor_points.reshape((K * A, 2))
+
+    return all_anchor_points
+
+# this class generate all reference points on all pyramid levels
+class AnchorPoints(nn.Module):
+    def __init__(self, pyramid_levels=None, strides=None, row=3, line=3, device='cpu'):
+        super(AnchorPoints, self).__init__()
+
+        if pyramid_levels is None:
+            self.pyramid_levels = [3, 4, 5, 6, 7]
+        else:
+            self.pyramid_levels = pyramid_levels
+
+        if strides is None:
+            self.strides = [2 ** x for x in self.pyramid_levels]
+
+        self.row = row
+        self.line = line
+        self.device = device
+
+    def forward(self, image):
+        image_shape = image.shape[2:]
+        image_shape = np.array(image_shape)
+        image_shapes = [(image_shape + 2 ** x - 1) // (2 ** x) for x in self.pyramid_levels]
+
+        all_anchor_points = np.zeros((0, 2)).astype(np.float32)
+        # get reference points for each level
+        for idx, p in enumerate(self.pyramid_levels):
+            anchor_points = generate_anchor_points(2**p, row=self.row, line=self.line)
+            shifted_anchor_points = shift(image_shapes[idx], self.strides[idx], anchor_points)
+            all_anchor_points = np.append(all_anchor_points, shifted_anchor_points, axis=0)
+
+        all_anchor_points = np.expand_dims(all_anchor_points, axis=0)
+        # send reference points to device
+        return torch.from_numpy(all_anchor_points.astype(np.float32)).to(self.device)
+
+class Decoder(nn.Module):
+    def __init__(self, C3_size, C4_size, C5_size, feature_size=256):
+        super(Decoder, self).__init__()
+
+        # upsample C5 to get P5 from the FPN paper
+        self.P5_1 = nn.Conv2d(C5_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P5_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
+        self.P5_2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, stride=1, padding=1)
+
+        # add P5 elementwise to C4
+        self.P4_1 = nn.Conv2d(C4_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P4_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
+        self.P4_2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, stride=1, padding=1)
+
+        # add P4 elementwise to C3
+        self.P3_1 = nn.Conv2d(C3_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P3_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
+        self.P3_2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, stride=1, padding=1)
+
+
+    def forward(self, inputs):
+        C3, C4, C5 = inputs
+
+        P5_x = self.P5_1(C5)
+        P5_upsampled_x = self.P5_upsampled(P5_x)
+        P5_x = self.P5_2(P5_x)
+
+        P4_x = self.P4_1(C4)
+        P4_x = P5_upsampled_x + P4_x
+        P4_upsampled_x = self.P4_upsampled(P4_x)
+        P4_x = self.P4_2(P4_x)
+
+        P3_x = self.P3_1(C3)
+        P3_x = P3_x + P4_upsampled_x
+        P3_x = self.P3_2(P3_x)
+
+        return [P3_x, P4_x, P5_x]
+
+# the defenition of the P2PNet model
+class P2PNet(nn.Module):
+    def __init__(self, backbone, row=1, line=1):
+        super().__init__()
+        self.backbone = backbone
+        self.num_classes = 2
+        # the number of all anchor points
+        num_anchor_points = row * line
+
+        self.regression = RegressionModel(num_features_in=256, num_anchor_points=num_anchor_points)
+        self.classification = ClassificationModel(num_features_in=256, \
+                                            num_classes=self.num_classes, \
+                                            num_anchor_points=num_anchor_points)
+
+        self.anchor_points = AnchorPoints(pyramid_levels=[3,], row=row, line=line)
+
+        self.fpn = Decoder(256, 512, 512)
+
+    def forward(self, samples):
+        # get the backbone features
+        features = self.backbone(samples)
+        # forward the feature pyramid
+        features_fpn = self.fpn([features[1], features[2], features[3]])
+
+        batch_size = features[0].shape[0]
+        # run the regression and classification branch
+        regression = self.regression(features_fpn[1]) * 100 # 8x
+        classification = self.classification(features_fpn[1])
+        anchor_points = self.anchor_points(samples).repeat(batch_size, 1, 1)
+        # decode the points as prediction
+        output_coord = regression + anchor_points
+        output_class = classification
+        out = {'pred_logits': output_class, 'pred_points': output_coord}
+       
+        return out
+    
+    def to(self, device):
+        ret = super().to(device)
+        self.anchor_points.device = device
+        return ret
+    
+def load_p2pnet(device='cpu'):
+    model = P2PNet(Backbone_VGG16BN()).to(device)
+    sd = torch.load(WEIGHT_FILE, map_location=device)
+    model.load_state_dict(sd)
+    return model
+
+### run inference
+class GrapeCounter:
+    def __init__(self, device='cpu', thresh=0.5):
+        self.model = load_p2pnet(device=device)
+        self.trf = transforms.Compose([
+            transforms.ToTensor(), 
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225]),
+        ])
+        self.thresh = thresh
+        self.device = device
+        self.pil_to_tensor = transforms.ToTensor()
+    
+    def _load_image(self, file):
+        img = Image.open(file)
+        width, height = img.size
+        new_width = width // 128 * 128
+        new_height = height // 128 * 128
+        img = img.resize((new_width, new_height), Image.LANCZOS)
+        return img
+    
+    def _get_points(self, outputs):
+        scores = F.softmax(outputs['pred_logits'].squeeze(), -1)[..., -1] # get scores for pos class
+        outputs_points = outputs['pred_points'].squeeze()
+        points = outputs_points[scores > self.thresh]
+        return points
+    
+    def _vis(self, img, points):
+        img_arr = self.pil_to_tensor(img.convert('RGB')).numpy() * 255
+        img_arr = img_arr.transpose([1, 2, 0])[:, :, ::-1].astype(np.uint8).copy()
+        print(img_arr.shape)
+        size = 2
+        for p in points:
+            img_arr = cv2.circle(img_arr, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
+        return img_arr
+    
+    def count(self, filepath, vis=False):
+        img = self._load_image(filepath)
+        x = self.trf(img).type(torch.float32).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            outputs = self.model(x)
+            points = self._get_points(outputs).cpu().numpy()
+            npoints = len(points)
+        if vis:
+            vis_img = self._vis(img, points)
+            ix = filepath.rfind('.')
+            cv2.imwrite(f'{filepath[:ix]}_pred{npoints}{filepath[ix:]}', vis_img)
+        return npoints
+
+def main(device='cpu'):
+    gc = GrapeCounter(device=device)
+    gc.count('testimage1.jpg', vis=True)
+    gc.count('testimage2.jpeg', vis=True)
+    gc.count('testimage3.jpg', vis=True)
+
+if __name__ == '__main__':
+    main()
+
